acee7b6aea25f468489290390499f08baa554a88
Kriti-59 <kritinewray@gmail.com>
2024-09-08 18:28:50 -0500

    Implement user authentication logic with JWT and bcrypt

---
+++ budget_bliss_api/controllers/AuthController.js
@@ -0,0 +1,37 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const db = require('../config/database'); 
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const sql = 'SELECT * FROM users WHERE email = ?';
+    const [result] = await db.promise().query(sql, [email]);
+
+    // Check if the user exists
+    if (result.length === 0) {
+      return res.status(400).json({ message: 'Invalid email or password' });
+    }
+
+    const user = result[0];
+
+    // Compare the entered password with the hashed password in the database
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: 'Invalid email or password' });
+    }
+
+    // Generate JWT token if login is successful
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    return res.json({ token });
+
+  } catch (err) {
+    console.error('Error during login:', err);
+    return res.status(500).send('Server error');
+  }
+};
+
+
+module.exports = { loginUser };
