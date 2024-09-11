14517169681707ae2e8b469c369f1fe16e8ee7d1
Kriti-59 <kritinewray@gmail.com>
2024-09-11 10:53:46 -0500

    create routes for login, dashboard and expenses

---
+++ budget_bliss_api/routes/routes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/AuthController');
+const ExpenseController = require('../controllers/ExpenseController')
+const db = require('../config/database');
+
+// Login route
+router.post('/login', authController.loginUser);
+
+// Dashboard route
+router.get('/dashboard', (req, res) => {
+  res.send('Welcome to the dashboard!');
+});
+
+// Route to get expenses
+router.get('/expenses', ExpenseController.getExpenses);
+
+module.exports = router;
