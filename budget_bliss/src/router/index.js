0ce7b27109833db7aaad11b1253bf7cb60a5912d
Kriti-59 <kritinewray@gmail.com>
2024-09-30 09:44:00 -0500

    Add path for logout functionality

--- budget_bliss/src/router/index.js
+++ budget_bliss/src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../views/Login.vue';
 import Dashboard from '../views/Dashboard.vue';
+import Logout from '../components/Logout.vue';
 
 const routes = [
   { path: '/login', component: Login },
-  { path: '/dashboard', component: Dashboard }
+  { path: '/dashboard', component: Dashboard },
+  { path: '/logout', component: Logout},
 ];
 
 const router = createRouter({
